05dd7849ac509e86343ec80b94658a1aecf218cf
Zach Parker <parkerz410@gmail.com>
2019-05-20 16:37:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
-  range = 2..(num-1)
-  range_array = range.to_a
     if num % i == 0
+      range = 2..(num-1)
+      range_array = range.to_a
       return false
     end
 end
